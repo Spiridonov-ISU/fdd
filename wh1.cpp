9c7a66a1ba196696065d6f274a8a7b889873b94e
Spiridonov-ISU <56498495+Spiridonov-ISU@users.noreply.github.com>
2019-10-31 01:51:54 +0800

    Add files via upload

---
+++ wh1.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//#include <conio.h>
+//#include <locale.h>
+//
+//using namespace std;
+//
+//int main(){
+//	setlocale(LC_ALL,"Russian");
+//	int x, i=1;
+//	cout<<"Введите число: ";
+//	cin>>x;
+//	cout<<"Делители этого числа: ";
+//		for(;;i++){
+//			if(x%i==0)
+//				cout<<x/i<<" ";
+//		}
+//	_getch();
+//	return 0;
+//}
