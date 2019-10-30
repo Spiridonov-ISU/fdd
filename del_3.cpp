39298732427966b2503c25fb66585ce23f5de2f1
Spiridonov-ISU <56498495+Spiridonov-ISU@users.noreply.github.com>
2019-10-31 01:58:59 +0800

    Add files via upload

---
+++ del_3.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//#include <conio.h>
+//#include <locale.h>
+//
+//using namespace std;
+//
+//int main(){
+//	setlocale(LC_ALL,"Russian");
+//	int x, sum=0;
+//	cout<<"Введите число: "; cin>>x;
+//	while(x>0){
+//		sum+=x%10;
+//		x/=10;
+//	}
+//	if ((sum)%3==0)
+//		cout<<"Делится на 3";
+//	else cout<<"Не делится на 3";
+//	_getch();
+//	return 0;
+//}
