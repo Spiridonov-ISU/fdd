45a0db38441af77269f9a8f5d7ab0b29415880b9
Spiridonov-ISU <56498495+Spiridonov-ISU@users.noreply.github.com>
2019-11-21 03:20:35 +0800

    Add files via upload

---
+++ wh10.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//#include <conio.h>
+//#include <locale.h>
+//
+//using namespace std;
+//
+//int main(){
+//	setlocale(LC_ALL,"Russian");
+//	int n, bac=1;
+//	cout<<"Задайте время в минутах: ";cin>>n;
+//	bac=2<<n-1;
+//	cout<<"Колличество бактерий: "<<bac;
+//	_getch();
+//	return 0;
+//}
